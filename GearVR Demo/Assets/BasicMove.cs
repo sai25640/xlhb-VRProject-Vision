087538d2e9e6119c80daaabb769afbcdc2d8cd7b
sai25640 <sai25640@163.com>
2017-04-27 15:58:20 +0800

    master 2017/4/27 water clock

--- GearVR Demo/Assets/BasicMove.cs
+++ GearVR Demo/Assets/BasicMove.cs
@@ -13,14 +13,14 @@ public class BasicMove : MonoBehaviour {
         yield return new WaitForSeconds(1f);
 
         m_Boat.DOMove(new Vector3(0, 3.3f, -7.5f), 2f).SetLoops(-1, LoopType.Yoyo);
-        m_Watch.DORotate(new Vector3(0,0,45f), 4f).SetLoops(-1, LoopType.Yoyo);
+        //m_Watch.DORotate(new Vector3(0,0,45f), 4f).SetLoops(-1, LoopType.Yoyo);
         //m_Watch.DOShakeRotation(4f).SetLoops(-1, LoopType.Yoyo);
 
-        if (SceneManager.GetActiveScene().name != "VRDemo_MeditationScene2")
-        {
-            m_Cave[0].DOLocalRotate(new Vector3(0, 0, 90f), 2f).SetLoops(-1, LoopType.Yoyo);
-            m_Cave[1].DOLocalRotate(new Vector3(0, 0, -90f), 2f).SetLoops(-1, LoopType.Yoyo);
-        }
+        //if (SceneManager.GetActiveScene().name != "VRDemo_MeditationScene2")
+        //{
+        //    m_Cave[0].DOLocalRotate(new Vector3(0, 0, 90f), 2f).SetLoops(-1, LoopType.Yoyo);
+        //    m_Cave[1].DOLocalRotate(new Vector3(0, 0, -90f), 2f).SetLoops(-1, LoopType.Yoyo);
+        //}
         
     }
 	
